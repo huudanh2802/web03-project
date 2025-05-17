f193b40723620ef37bcf80687364ad9665d3c94f
Danh Hữu <56262591+huudanh2802@users.noreply.github.com>
2025-05-17 06:26:09 +0000

    May 17, 2025 at 1:26:09 PM GMT+7

--- web03-app/lib/bloc/note_bloc.dart
+++ web03-app/lib/bloc/note_bloc.dart
@@ -51,6 +51,7 @@ class NoteBloc extends Bloc<NoteEvent, NoteState> {
 
   void _onUpdateNote(UpdateNote event, Emitter<NoteState> emit) async {
     try {
+      print(event.note.note);
       final updatedNotes =
           state.notes.map((note) {
             return note.id == event.note.id ? event.note : note;
